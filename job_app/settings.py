105e176bed5db2b882fa92c0dc87e7b3348d6af8
Vishal Vishwakarma <officialvishavant@gmail.com>
2023-11-07 11:52:05 +0530

    My SQL database Added

--- job_app/settings.py
+++ job_app/settings.py
@@ -89,12 +89,27 @@ WSGI_APPLICATION = 'job_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# Connecting MySQL database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'workzendatabase',
+        'USER': 'dbadmin',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
+
+
 
 
 # Password validation
